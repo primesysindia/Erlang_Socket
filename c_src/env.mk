07b81454f5840cf07997be5972f1ce95d0803d4d
Piyush Sable <piyush.sable@primesystech.com>
2016-07-26 11:03:34 +0530

    Added C SO output files to the Repo.

--- c_src/env.mk
+++ c_src/env.mk
@@ -1,3 +1,3 @@
-ERTS_INCLUDE_DIR ?= /usr/lib/erlang/erts-6.3/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /usr/lib/erlang/lib/erl_interface-3.7.20/include
-ERL_INTERFACE_LIB_DIR ?= /usr/lib/erlang/lib/erl_interface-3.7.20/lib
+ERTS_INCLUDE_DIR ?= /usr/lib/erlang/erts-7.0/include/
+ERL_INTERFACE_INCLUDE_DIR ?= /usr/lib/erlang/lib/erl_interface-3.8/include
+ERL_INTERFACE_LIB_DIR ?= /usr/lib/erlang/lib/erl_interface-3.8/lib
